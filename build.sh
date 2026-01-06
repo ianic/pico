18319af3ab70325486c92fa3a4bb23689a7fb970
Igor Anić <igor.anic@gmail.com>
2026-01-06 23:27:24 +0100

    add logging and uart command for bootsel

--- build.sh
+++ build.sh
@@ -4,8 +4,8 @@ set -e
 zig build -Doptimize=ReleaseSafe
 
 # send uart reboot command
-#printf '\xAB' | sudo tio -b 115200 /dev/ttyACM0
-#sleep 0.5
+printf '\xAB' | sudo tio -b 115200 /dev/ttyACM0
+sleep 0.5
 
 app=${1:-blinky}
 picotool load -x -f zig-out/firmware/"$app".uf2
--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,47 @@
 const std = @import("std");
 const microzig = @import("microzig");
-const rp2xxx = microzig.hal;
-const gpio = rp2xxx.gpio;
-const time = rp2xxx.time;
+
+const hal = microzig.hal;
+const gpio = hal.gpio;
+const time = hal.time;
+const uart = hal.uart.instance.num(0);
+
+pub const microzig_options = microzig.Options{
+    .log_level = .debug,
+    .logFn = hal.uart.log,
+};
 
 // Compile-time pin configuration
-const pin_config = rp2xxx.pins.GlobalConfiguration{
+const pin_config = hal.pins.GlobalConfiguration{
+    .GPIO0 = .{ .function = .UART0_TX },
+    .GPIO1 = .{ .function = .UART0_RX },
     .GPIO15 = .{ .name = "led", .direction = .out, .function = .SIO },
 };
 const pins = pin_config.pins();
 
 pub fn main() !void {
     pin_config.apply();
-    const led = pins.led;
+    // init uart logging
+    uart.apply(.{ .clock_config = hal.clock_config });
+    hal.uart.init_logger(uart);
 
+    const led = pins.led;
     while (true) {
         led.toggle();
-        time.sleep_ms(250);
+        time.sleep_ms(500);
+        check_reboot();
+    }
+}
+
+// Puts pico in bootsel mode by uart command.
+fn check_reboot() void {
+    const MAGICREBOOTCODE: u8 = 0xAB;
+    const v = uart.read_word() catch {
+        uart.clear_errors();
+        return;
+    } orelse return;
+    if (v == MAGICREBOOTCODE) {
+        std.log.warn("Reboot cmd received", .{});
+        hal.rom.reset_to_usb_boot();
     }
 }
