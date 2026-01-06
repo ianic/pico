9dd3a223710d19933a5ed0695d598149942a574f
Igor Anić <igor.anic@gmail.com>
2026-01-06 23:01:43 +0100

    add blinky

---
+++ build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+set -e
+
+zig build -Doptimize=ReleaseSafe
+
+# send uart reboot command
+#printf '\xAB' | sudo tio -b 115200 /dev/ttyACM0
+#sleep 0.5
+
+app=${1:-blinky}
+picotool load -x -f zig-out/firmware/"$app".uf2
