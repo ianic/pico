4212c1ce7abe9e54a8e66730fbb188f3571980da
Igor Anić <igor.anic@gmail.com>
2026-02-02 13:51:11 +0100

    pfs iteration with names

--- src/pfs.zig
+++ src/pfs.zig
@@ -1,8 +1,19 @@
 const std = @import("std");
 const assert = std.debug.assert;
 
+const Entry = extern struct {
+    name: [12]u8 = @splat(0),
+    size: u32 = 0,
+
+    fn sizeWithPadding(self: @This()) u32 {
+        const padding: u8 = (4 - @as(u8, @intCast(self.size & 0b11)) & 0b11);
+        return self.size + padding;
+    }
+};
+
 test "load" {
     const testing = std.testing;
+    const gpa = testing.allocator;
 
     const files: []const []const u8 = &.{
         "../../../../Code/microzig/drivers/wireless/cyw43/firmware/43439A0_7_95_61.bin",
@@ -10,61 +21,78 @@ test "load" {
         "../../../../Code/microzig/drivers/wireless/cyw43/firmware/43439A0_clm.bin",
     };
 
-    const gpa = testing.allocator;
+    var fat = try std.Io.Writer.Allocating.initCapacity(gpa, 4);
+    var fw = &fat.writer;
+    defer fat.deinit();
 
-    var header = try std.Io.Writer.Allocating.initCapacity(gpa, 4);
-    var hw = &header.writer;
-
-    var content = std.Io.Writer.Allocating.init(gpa);
-    var cw = &content.writer;
-
-    // var header: []u8 = gpa.alloc(u8, 4);
-    // var header_pos: usize = 4;
-    // var content: []u8 = &.{};
-    // var content_pos: usize = 0;
-    // defer gpa.free(content);
+    var blob = std.Io.Writer.Allocating.init(gpa);
+    var bw = &blob.writer;
+    defer blob.deinit();
 
     var offset: u32 = 0;
     for (files) |path| {
-        const basename = std.fs.path.basename(path);
+        const basename = std.fs.path.basename(path)[8..];
         const bytes = try std.fs.cwd().readFileAlloc(gpa, path, 1024 * 1024); // 1MB
         defer gpa.free(bytes);
         std.debug.print("{s} {d} {s}\n", .{ basename, bytes.len, path });
 
-        try hw.ensureUnusedCapacity(1 + basename.len + 4 + 4);
-        try hw.writeByte(@intCast(basename.len));
-        try hw.writeAll(basename);
-        try hw.writeInt(u32, offset, .little);
-        try hw.writeInt(u32, @intCast(bytes.len), .little);
+        try fw.ensureUnusedCapacity(@sizeOf(Entry));
+        var entry: Entry = .{ .size = @intCast(bytes.len) };
+        const name_len = @min(basename.len, entry.name.len);
+        @memcpy(entry.name[0..name_len], basename[0..name_len]);
+        try fw.writeAll(std.mem.asBytes(&entry));
+
         offset += @intCast(bytes.len);
 
-        try cw.ensureUnusedCapacity(bytes.len);
-        try cw.writeAll(bytes);
-
-        // header = try gpa.realloc(content, header_pos + 1 + basename.len + 4 + 4);
-        // header[header_pos] = @intCast(basename.len);
-        // content_pos += 1;
-        // @memcpy(content[content_pos..][0..basename.len], basename);
-
-        // content = try gpa.realloc(content, content_pos + 1 + basename.len + 4 + bytes.len);
-
-        // content_pos += basename.len;
-        // std.mem.writeInt(u32, content[content_pos..][0..4], @intCast(bytes.len), .little);
-        // content_pos += 4;
-        // @memcpy(content[content_pos..][0..bytes.len], bytes);
-        // content_pos += bytes.len;
-        // assert(content_pos == content.len);
+        const padding: u8 = (4 - @as(u8, @intCast(bytes.len & 0b11)) & 0b11);
+        try bw.ensureUnusedCapacity(bytes.len + padding);
+        try bw.writeAll(bytes);
+        for (0..padding) |_| {
+            try bw.writeByte(0);
+        }
     }
 
-    const header_buf = try header.toOwnedSlice();
-    std.mem.writeInt(u32, header_buf[0..4], @intCast(header_buf.len), .little);
-    const content_buf = try content.toOwnedSlice();
-    defer gpa.free(header_buf);
-    defer gpa.free(content_buf);
+    try fw.ensureUnusedCapacity(@sizeOf(Entry));
+    const entry: Entry = .{};
+    try fw.writeAll(std.mem.asBytes(&entry));
 
     const output = try std.fs.cwd().createFile("pfs.bin", .{});
     defer output.close();
     var ow = output.writer(&.{});
-    try ow.interface.writeAll(header_buf);
-    try ow.interface.writeAll(content_buf);
+    try ow.interface.writeAll(fat.written());
+    try ow.interface.writeAll(blob.written());
+}
+
+test "parse" {
+    const testing = std.testing;
+    const gpa = testing.allocator;
+    const path = "pfs.bin";
+
+    const bytes = try std.fs.cwd().readFileAlloc(gpa, path, 4 * 1024 * 1024);
+    defer gpa.free(bytes);
+
+    var rdr = std.Io.Reader.fixed(bytes);
+
+    const name = "7_95_61.bin";
+    var size: u32 = 0;
+    var offset: u32 = 0;
+    var entries: u32 = 0;
+    while (true) {
+        const entry = try rdr.takeStruct(Entry, .little);
+        entries += 1;
+        if (entry.size == 0) break;
+        if (size > 0) continue;
+        if (std.mem.eql(u8, name, entry.name[0..name.len])) {
+            size = entry.size;
+            continue;
+        }
+        std.debug.print(
+            "entry: {s} size: {} withPadding: {} offset: {}\n",
+            .{ entry.name, entry.size, entry.sizeWithPadding(), offset },
+        );
+        offset += entry.sizeWithPadding();
+    }
+    std.debug.print("{} {} {}\n", .{ size, entries, offset });
+
+    std.debug.print("{x}\n", .{bytes[@sizeOf(Entry) * entries ..][offset..][0..size][0..64]});
 }
